adf08455a947c58d195d53a3f09f4c24c6cdcf16
Default Name <default@example.com>
2025-09-05 01:54:31 +0800

    design for sidebar works well

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
